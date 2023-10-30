2b43603078bd75f5003b738fe0ab8da45c0c5214
Luciann-Erlan <luciann.erlan@gmail.com>
2023-10-29 22:03:24 -0300

    meu segundo código, aperfeiçoado

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-let heroName = 'Luciann Erlãn'; // Coloque o nome do herói aqui
-let xp = 3100; // Coloque o xp do herói aqui
+// Solicita ao usuário o nome do herói e o XP
+var nomeHeroi = prompt("Digite o nome do herói:");
+var xp = parseInt(prompt("Digite a quantidade de experiência (XP) do herói:"));
 
-let level = '';
+// Verifica o nível do herói com base no XP
+var nivelHeroi;
 
 if (xp < 1000) {
-    level = "ferro";
-} else if (xp>=1001 && xp<=2000){
-    level = "bronze";
-} else if (xp>=2001 && xp<=5000){
-    level = "prata";
-} else if (xp>=6001 && xp<=7000){
-    level = "ouro";
-} else if (xp>=5001 && xp<=8000){
-    level = "platina";
-} else if (xp>=8001 && xp<=9000){
-    level = "ascendente";
-} else if (xp>=9001 && xp<=10000){
-    level = "imortal";
-} else if (xp>=10001){
-    level = "radiante";
+  nivelHeroi = "ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+  nivelHeroi = "bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+  nivelHeroi = "prata";
+} else if (xp >= 6001 && xp <= 7000) {
+  nivelHeroi = "ouro";
+} else if (xp >= 5001 && xp <= 8000) {
+  nivelHeroi = "platina";
+} else if (xp >= 8001 && xp <= 9000) {
+  nivelHeroi = "ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+  nivelHeroi = "imortal";
+} else if (xp >= 10001) {
+  nivelHeroi = "radiante";
 }
 
-console.log("O herói de nome " + heroName + " está no nível " + level);
+// Exibe a mensagem com o nome e o nível do herói
+console.log("O herói de nome " + nomeHeroi + " está no nível " + nivelHeroi);
